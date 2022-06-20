036de0481a5685287f240400afbb804b30f95f67
Ricardo Arturo <49619270+Ricaidito@users.noreply.github.com>
2022-06-20 11:14:18 -0400

    Updated model

--- api/models/job.model.js
+++ api/models/job.model.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
 
-const jobSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  company: String,
-  type: String,
-});
+const jobSchema = mongoose.Schema(
+  {
+    _id: mongoose.Schema.Types.ObjectId,
+    company: String,
+    type: String,
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Job", jobSchema);

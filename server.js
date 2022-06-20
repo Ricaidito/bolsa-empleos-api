bf9cb67347c5a742092983a3243cf7fa80512478
Ricardo Arturo <49619270+Ricaidito@users.noreply.github.com>
2022-06-19 20:59:55 -0400

    Polished listen method

--- server.js
+++ server.js
@@ -4,4 +4,6 @@ const app = require("./app");
 const port = process.env.PORT || 8080;
 const server = http.createServer(app);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`>>> API: http://localhost:${port} <<<`);
+});
